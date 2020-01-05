70483c2ed593d2f57eb4def6efdd8312d6da4672
loocasch <loocasch@gmail.com>
2020-01-05 16:11:23 +0100

    zadanie 3 Cross Validation

--- c03.py
+++ c03.py
@@ -2,14 +2,13 @@ import numpy
 import random
 
 from helpers import *
-from c02 import groupDecisions, getNaiveBayes
 
 
 def TrainAndTest(lists, ratio):
     tmpDictionary = {}
     lengthRowList = len(lists)
     lengthColumnLists = len(lists[0])
-    random.shuffle(lists)
+    lists = random.sample(lists, lengthRowList)
     tmpDictionary['train'] = lists[:int(lengthRowList * ratio)]
     tmpDictionary['test'] = lists[int(lengthRowList * ratio):lengthRowList]
     return tmpDictionary
@@ -19,12 +18,38 @@ def MonteCarloCrossValidation(lists, numberFold, ratio):
     sumGlobalAccuracy = 0
     sumBalancedAccuracy = 0
     for fold in range(numberFold):
-        x = getNaiveBayes(TrainAndTest(data, 0.5)['test'], TrainAndTest(data, 0.5)['train'], "MonteCarloCrossValidationFold%s" % (fold + 1), True)
-        sumGlobalAccuracy += x[0]
-        sumBalancedAccuracy += x[1]
+        naiveBayes = getNaiveBayes(TrainAndTest(data, 0.5)['test'], TrainAndTest(data, 0.5)['train'], "MonteCarloCrossValidationFold", True)
+        sumGlobalAccuracy += naiveBayes[0]
+        sumBalancedAccuracy += naiveBayes[1]
     acc_bayes = open("data/MonteCarloCrossValidationAverage_acc_bayes.txt", "w+")
     acc_bayes.write('Average Global Accuracy: {}\n'.format(sumGlobalAccuracy / numberFold))
     acc_bayes.write('Average Balanced Accuracy: {}\n'.format(sumBalancedAccuracy / numberFold))
+    acc_bayes.close()
+
+
+def CrossValidation(lists, k):
+    lengthRowList = len(lists)
+    n = int(lengthRowList / k)
+    lists = random.sample(lists, lengthRowList)
+    chunkLists = [lists[i:i + n] for i in range(0, len(lists), n)]
+    for index, li in enumerate(chunkLists):
+        if(len(li) != n):
+            chunkLists.pop(index)
+    sumGlobalAccuracy = 0
+    sumBalancedAccuracy = 0
+    for tst in chunkLists:
+        trainList = []
+        for trn in chunkLists:
+            if(tst != trn):
+                for t in trn:
+                    trainList.append(t)
+
+        naiveBayes = getNaiveBayes(tst, trainList, "CrossValidation", True)
+        sumGlobalAccuracy += naiveBayes[0]
+        sumBalancedAccuracy += naiveBayes[1]
+    acc_bayes = open("data/CrossValidationAverage_acc_bayes.txt", "w+")
+    acc_bayes.write('Average Global Accuracy: {}\n'.format(sumGlobalAccuracy / k))
+    acc_bayes.write('Average Balanced Accuracy: {}\n'.format(sumBalancedAccuracy / k))
     acc_bayes.close()
 
 
@@ -34,3 +59,5 @@ TrainAndTestLists = TrainAndTest(data, 0.5)
 getNaiveBayes(TrainAndTestLists['test'], TrainAndTestLists['train'], "TrainAndTest", False)
 
 MonteCarloCrossValidation(data, 5, 0.5)
+
+CrossValidation(data, 5)
--- helpers.py
+++ helpers.py
@@ -1,3 +1,7 @@
+import random
+import numpy
+
+
 def getClassDecisions(lists):
     tmpList = []
     for index, li in enumerate(lists, start=1):
@@ -54,3 +58,86 @@ def importFile(path):
             diabetes[index].append(float(n))
 
     return diabetes
+
+
+def groupDecisions(decisions):
+    tmpDictionary = {}
+    sumCorectDecision = 0
+    for decision in decisions:
+        if(decisions[decision]['dec'] not in tmpDictionary):
+            tmpDictionary[decisions[decision]['dec']] = {'correct': 0, 'count': 1}
+        else:
+            tmpDictionary[decisions[decision]['dec']]['count'] += 1
+        if(decisions[decision]['correct']):
+            tmpDictionary[decisions[decision]['dec']]['correct'] += 1
+    for key in tmpDictionary:
+        sumCorectDecision += tmpDictionary[key]['correct'] / tmpDictionary[key]['count']
+    return sumCorectDecision
+
+
+def getNaiveBayes(tst, train, nameFile, getGaccBacc):
+    classDecisions = getClassDecisions(train)
+    lengthClassDecisions = getLengthClassDecision(train, classDecisions)
+    lengthRow = len(train)
+    atributes = getTransposeAllAtributesList(train)
+    counts = {}
+    decisions = {}
+    correctDecision = 0
+
+    for index1, row in enumerate(tst, start=1):
+        counts['x{}'.format(index1)] = {}
+        for classDecision in classDecisions:
+            counts['x{}'.format(index1)][classDecision] = {}
+            for index2, item in enumerate(row[:-1]):
+                counts['x{}'.format(index1)][classDecision]['a{}'.format(index2)] = 0
+                count = 0
+                for index3, atribute in enumerate(atributes[index2]):
+                    if(item == atribute and classDecision == atributes[-1][index3]):
+                        count += 1
+                counts['x{}'.format(index1)][classDecision]['a{}'.format(index2)] = count
+
+    for index1, count in enumerate(counts):
+        for classDecision in counts[count]:
+            for item in counts[count][classDecision]:
+                if(counts[count][classDecision][item] == 0):
+                    for itemClassDecision in counts[count]:
+                        if(counts[count][itemClassDecision][item] != 0):
+                            counts[count][itemClassDecision][item] += 1
+            counts[count][classDecision]['p'] = lengthClassDecisions[classDecision] / lengthRow * (sum(counts[count][classDecision].values()) / lengthClassDecisions[classDecision])
+
+        prevItem = None
+        for index2, classDecision in enumerate(counts[count]):
+            if(prevItem == None):
+                prevItem = [counts[count][classDecision]['p'], classDecision]
+                continue
+            else:
+                decisions[count] = {}
+                if(prevItem[0] > counts[count][classDecision]['p']):
+                    decisions[count]['dec'] = prevItem[1]
+                elif(prevItem[0] == counts[count][classDecision]['p']):
+                    decisions[count]['dec'] = random.choice(classDecisions)
+                else:
+                    decisions[count]['dec'] = classDecision
+            if(decisions[count]['dec'] == tst[index1][-1]):
+                decisions[count]['correct'] = True
+                correctDecision += 1
+            else:
+                decisions[count]['correct'] = False
+
+    if(getGaccBacc):
+        globalAccuracy = correctDecision / len(decisions)
+        balancedAccuracy = groupDecisions(decisions) / len(classDecisions)
+        return globalAccuracy, balancedAccuracy
+    else:
+        dec_bayes = open("data/%s_dec_bayes.txt" % nameFile, "w+")
+        dec_bayes.write("Obiekt tst\t\tUkryta decyzja eksperta\t\tDecyzja naszego klasyfikatora\t\tPoprawnie?\n")
+        dec_bayes.write("-------------------------------------------------------------------------------------------------------------\n")
+        for index, decision in enumerate(decisions):
+            dec_bayes.write("%s\t\t\t\t%f\t\t\t\t%s\t\t\t%s\n" % (decision, tst[index][-1], decisions[decision]['dec'], 'Tak' if decisions[decision]['correct'] else 'Nie'))
+        dec_bayes.write("-------------------------------------------------------------------------------------------------------------\n")
+        dec_bayes.close()
+
+        acc_bayes = open("data/%s_acc_bayes.txt" % nameFile, "w+")
+        acc_bayes.write('Global Accuracy: {}\n'.format(correctDecision / len(decisions)))
+        acc_bayes.write('Balanced Accuracy: {}\n'.format(groupDecisions(decisions) / len(classDecisions)))
+        acc_bayes.close()
